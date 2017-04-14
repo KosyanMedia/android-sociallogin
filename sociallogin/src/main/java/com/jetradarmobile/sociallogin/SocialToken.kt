a1fc5fa6a1531deb1007f868142cabc34a0dcf80
Artemii Vishnevskii <temaa.mann@gmail.com>
2017-04-14 17:22:42 +0300

    added token data class

---
+++ sociallogin/src/main/java/com/jetradarmobile/sociallogin/SocialToken.kt
@@ -0,0 +1,16 @@
+package com.jetradarmobile.sociallogin
+
+/**
+ * Data class that store user data from social network
+ *
+ * @property token authorization token provided by social network api
+ * @property secret secret hash
+ * @property userId user identifier in social network
+ * @property userName user display name
+ * @property email user email
+ */
+data class SocialToken(val token: String,
+                       val secret: String,
+                       val userId: String,
+                       val userName: String,
+                       val email: String)
