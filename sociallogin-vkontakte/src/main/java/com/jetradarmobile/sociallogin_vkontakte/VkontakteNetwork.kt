61b55b313bd4fc1d3534d6f27ec81d734757438d
Artemii Vishnevskii <temaa.mann@gmail.com>
2017-04-14 18:43:08 +0300

    added vkontakte network

---
+++ sociallogin-vkontakte/src/main/java/com/jetradarmobile/sociallogin_vkontakte/VkontakteNetwork.kt
@@ -0,0 +1,24 @@
+package com.jetradarmobile.sociallogin_vkontakte
+
+import android.content.Intent
+import com.jetradarmobile.sociallogin.SocialLoginCallback
+import com.jetradarmobile.sociallogin.SocialLoginFragment
+import com.jetradarmobile.sociallogin.SocialNetwork
+
+
+class VkontakteNetwork: SocialNetwork {
+
+
+
+    override fun login(socialLoginFragment: SocialLoginFragment, callback: SocialLoginCallback) {
+
+    }
+
+    override fun logout() {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+    }
+}
