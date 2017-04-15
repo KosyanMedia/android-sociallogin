00dd23486df736d6a48839fcb236ed04a9c94527
Artemii Vishnevskii <temaa.mann@gmail.com>
2017-04-15 11:46:18 +0300

    added vkontakte auth logic

--- sociallogin-vkontakte/src/main/java/com/jetradarmobile/sociallogin_vkontakte/VkontakteNetwork.kt
+++ sociallogin-vkontakte/src/main/java/com/jetradarmobile/sociallogin_vkontakte/VkontakteNetwork.kt
@@ -4,14 +4,21 @@ import android.content.Intent
 import com.jetradarmobile.sociallogin.SocialLoginCallback
 import com.jetradarmobile.sociallogin.SocialLoginFragment
 import com.jetradarmobile.sociallogin.SocialNetwork
+import com.jetradarmobile.sociallogin.SocialToken
+import com.vk.sdk.VKAccessToken
+import com.vk.sdk.VKCallback
+import com.vk.sdk.VKSdk
+import com.vk.sdk.api.VKError
+import java.lang.ref.WeakReference
 
 
-class VkontakteNetwork: SocialNetwork {
+class VkontakteNetwork : SocialNetwork, VKCallback<VKAccessToken> {
 
-
+    private var loginCallback: WeakReference<SocialLoginCallback>? = null
 
     override fun login(socialLoginFragment: SocialLoginFragment, callback: SocialLoginCallback) {
-
+        loginCallback = WeakReference(callback)
+        VKSdk.login(socialLoginFragment, "access_token")
     }
 
     override fun logout() {
@@ -19,6 +26,30 @@ class VkontakteNetwork: SocialNetwork {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        VKSdk.onActivityResult(requestCode, resultCode, data, this)
+    }
 
+    override fun onResult(token: VKAccessToken?) {
+        loginCallback?.get()?.let {
+            if (token != null) {
+                it.onLoginSuccess(this, createSocialToken(token))
+            } else {
+                it.onLoginError(this, "Vkontakte token receiving error")
+            }
+        }
+    }
+
+    override fun onError(error: VKError?) {
+        loginCallback?.get()?.onLoginError(this, error?.errorMessage ?: "Vkontakte authorization error")
+    }
+
+    private fun createSocialToken(vkAccessToken: VKAccessToken?): SocialToken {
+        return SocialToken(
+                token = vkAccessToken?.accessToken ?: "",
+                secret = vkAccessToken?.secret ?: "",
+                userId = vkAccessToken?.userId ?: "",
+                userName = "",
+                email = vkAccessToken?.email ?: ""
+        )
     }
 }
