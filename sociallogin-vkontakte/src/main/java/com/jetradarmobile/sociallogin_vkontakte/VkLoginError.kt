6087ece46f7f7a47233e843ba77096e6e340b81e
Artemii Vishnevskii <temaa.mann@gmail.com>
2017-05-22 14:34:36 +0300

    renames

--- sociallogin-vkontakte/src/main/java/com/jetradarmobile/sociallogin_vkontakte/VkLoginError.kt
+++
@@ -1,8 +0,0 @@
-package com.jetradarmobile.sociallogin_vkontakte
-
-import com.jetradarmobile.sociallogin.SocialLoginError
-
-
-class VkLoginError(reason: Reason) : SocialLoginError(reason) {
-    object NO_LOGIN : Reason("Vk token receiving error")
-}
